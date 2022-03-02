63986f4b7d6d44c7b3fa3581dc5fd7d72bca28c9
@basedest <basedscientist@gmail.com>
2022-03-02 23:37:57 +0300

    removed cringe

--- lab1/app.js
+++ lab1/app.js
@@ -1,32 +1,30 @@
 //используем фреймворк Express
 const express = require("express");
+const path = require('path');
 const app = express();
-//Используем для отображения ответа с сервера шаблоны EJS
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/views'));
 
-app.get("/", (req, res) => res.render('index'));
+console.log(path.join(__dirname, 'index.html'));
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 //Обработка GET-запроса
 app.get("/submit", (req, res) => {
-    let tmp = req.query;
-    console.log(tmp); 
-    let data = `
-    ФИО: ${tmp.lastName} ${tmp.firstName} ${tmp.middleName} 
-    Возраст: ${tmp.age} 
-    Телефон: ${tmp.phone} 
-    Эл. Почта: ${tmp.email} 
-    О себе: ${tmp.about} 
-    Опыт работы: ${tmp.experience} 
-    Образование: ${tmp.education} 
-    Навыки: 
-        ${tmp.hasOwnProperty("htmlCSS") ? "HTML и CSS": ""}
-        ${tmp.hasOwnProperty("js") ? "Javascript": ""}
-        ${tmp.hasOwnProperty("ps") ? "Adobe Photoshop": ""}
-    Планируемый заработок: ${tmp.salary}
-    Планирует работать ${tmp.hours} часов 
-    `;
-    res.render('submit', {res: data});
-}); 
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+    res.end([
+    'ФИО: ' + req.query.lastName + req.query.firstName + req.query.middleName, 
+    'Возраст: ' + req.query.age, 
+    'Телефон: ' + req.query.phone, 
+    'Эл. Почта: ' +  req.query.email, 
+    'О себе: ' + req.query.about,
+    'Опыт работы: ' + req.query.experience, 
+    'Образование: ' + req.query.education, 
+    'Навыки: ' +
+        + req.query.hasOwnProperty("htmlCSS") ? "HTML и CSS ": ""
+        + req.query.hasOwnProperty("js") ? "Javascript ": ""
+        + req.query.hasOwnProperty("ps") ? "Adobe Photoshop ": "",
+    'Планируемый заработок: ' + req.query.salary, 
+    'Планирует работать ' + req.query.hours + ' часов', 
+    ].join('\n'));
+});
 
 app.listen(3000, () => console.log("Сервер запущен..."));
